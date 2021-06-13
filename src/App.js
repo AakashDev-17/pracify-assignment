1bfe9b59d5b6f938e56ce69bb31de4ce48b9dbc3
Aakash Dev <75657300+AakashDev-17@users.noreply.github.com>
2021-06-13 16:20:38 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Section1 from "./pages/home/section1/Section1";
 import Section2 from "./pages/home/section2/Section2";
@@ -6,7 +5,6 @@ import Section3 from "./pages/home/section3/Section3";
 import Section4 from "./pages/home/section4/Section4";
 import Section5 from "./pages/home/section5/Section5";
 import Footer from "./components/footer/Footer";
-
 
 function App() {
   return (
